a967e8204ab00e1a3ddbafe2b656cd881fc094a3
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2024-01-25 13:06:38 +0100

    refactor: get product data

--- testdjangoapp/views.py
+++ testdjangoapp/views.py
@@ -6,20 +6,23 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Product
 from .forms import ProductForm
 
+def get_product_data(product):
+    return {
+        'id': product.id, 
+        'name': product.name, 
+        'description': product.description,
+        'price': product.price,
+        'category': product.category,
+        'quantity': product.quantity,
+        'created_at': product.created_at,
+        'updated_at': product.updated_at,
+    }
+
 @csrf_exempt
 def product_list(request):
     if request.method == 'GET':
         products = Product.objects.all()
-        product_data = [{
-            'id': product.id, 
-            'name': product.name, 
-            'description': product.description,
-            'price': product.price,
-            'category': product.category,
-            'quantity': product.quantity,
-            'created_at': product.created_at,
-            'updated_at': product.updated_at,
-        } for product in products]
+        product_data = [get_product_data(product) for product in products]
 
         return JsonResponse({
             'success': True,
@@ -30,16 +33,8 @@ def product_list(request):
 def product_detail(request, pk):
     if request.method == 'GET':
         product = Product.objects.get(pk=pk)
-        product_data = {
-            'id': product.id, 
-            'name': product.name, 
-            'description': product.description,
-            'price': product.price,
-            'category': product.category,
-            'quantity': product.quantity,
-            'created_at': product.created_at,
-            'updated_at': product.updated_at,
-        }
+        product_data = get_product_data(product)
+
         return JsonResponse({
             'success': True,
             'data': product_data,
@@ -53,16 +48,8 @@ def product_new(request):
             form = ProductForm(data)
             if form.is_valid():
                 product = form.save()
-                product_data = {
-                    'id': product.id, 
-                    'name': product.name, 
-                    'description': product.description,
-                    'price': product.price,
-                    'category': product.category,
-                    'quantity': product.quantity,
-                    'created_at': product.created_at,
-                    'updated_at': product.updated_at,
-                }
+                product_data = get_product_data(product)
+
                 return JsonResponse({
                         'success': True,
                         'data': product_data,
@@ -87,16 +74,8 @@ def product_edit(request, pk):
             form = ProductForm(data, instance=product)
             if form.is_valid():
                 product = form.save()
-                product_data = {
-                    'id': product.id, 
-                    'name': product.name, 
-                    'description': product.description,
-                    'price': product.price,
-                    'category': product.category,
-                    'quantity': product.quantity,
-                    'created_at': product.created_at,
-                    'updated_at': product.updated_at,
-                }
+                product_data = get_product_data(product)
+
                 return JsonResponse({
                         'success': True,
                         'data': product_data,
@@ -118,6 +97,7 @@ def product_delete(request, pk):
     if request.method == 'DELETE':
         product = Product.objects.get(pk=pk)
         product.delete()
+        
         return JsonResponse({
             'status': True,
             'data': 'Product successfully deleted',
